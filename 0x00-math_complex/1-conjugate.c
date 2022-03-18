17aa2206d9eb23be3eb613291445e323f140b44d
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-03-18 09:54:38 +0200

    conjugate file

---
+++ 0x00-math_complex/1-conjugate.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * conjugate - Returns the conjugate of an imaginary number
+ *
+ * @c: Complex number
+ * Return: Conjugate of c
+ */
+
+complex conjugate(complex c)
+{
+  c.im *= -1;
+  return(c);
+}
