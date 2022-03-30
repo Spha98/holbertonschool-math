affd55fe40372ce907805cb66f8d1280544d1f13
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-03-30 15:18:10 +0200

    Adding files

--- 0x01-math_sequence/2-mandelbrot.c
+++ 0x01-math_sequence/2-mandelbrot.c
@@ -1,42 +1,47 @@
 #include <stdio.h>
-#include <curses.h>
+
 /**
- * main - Function that prints Mandelbrot’s Sets
- *
- * Return: End Program
+ * main - mandelbrot
+ * Return: 0
  */
 int main(void)
 {
-	int A, B, i;
-	double a, b, x, y, t, n = 20;
+	int A, B, i, z = 100;
+	int w, h;
+	double a, b, x, y, r;
+	double n = 100;
+	FILE *pgmimg;
 
-	printf("Mandelbrot Set\n");
-
-	for (B = 0; B <= 4 * n; B++)
+	w = z * 4, h = z * 4;
+	printf("Holberton School\n");
+	printf("Mandelbrot's set image created in mandelbrot.pgm file\n");
+	pgmimg = fopen("mandelbrot.pgm", "wb");
+	fprintf(pgmimg, "P2\n");
+	fprintf(pgmimg, "%d %d\n", w, h);
+	fprintf(pgmimg, "255\n");
+	for (B = 0; B < 4 * n; B++)
 	{
 		b = 2 - (B / n);
-		for (A = 0; A <= 4 * n; A++)
+		for (A = 0; A < 4 * n; A++)
 		{
 			a = -2 + (A / n);
 			x = 0;
 			y = 0;
-
-			for (i = 1; i <= 1000; i++)
+			for (i = 1; i <= 1024; i++)
 			{
-				t = x;
+				r = x;
 				x = (x * x) - (y * y) + a;
-				y = (2 * t * y) + b;
+				y = (2 * r * y) + b;
 				if ((x * x) + (y * y) > 4)
 					break;
 			}
-
-			if (i == 1001)
-				printf(".");
+			if (i == 1025)
+				fprintf(pgmimg, "255 ");
 			else
-				printf(" ");
+				fprintf(pgmimg, "20 ");
 		}
-		printf("\n");
+		fprintf(pgmimg, "\n");
 	}
-	getch();
+	fclose(pgmimg);
 	return (0);
 }
